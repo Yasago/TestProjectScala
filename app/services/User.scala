37000c1e9d9c71a0bfe1598e0a3030cb8744d68a
yasago <looorrrd@mail.ru>
2020-10-31 01:23:34 +0500

    Finally created CRUD

--- app/services/User.scala
+++ app/services/User.scala
@@ -1,6 +1,25 @@
 package services
 
-case class User (var name: String, var login: String, var mail: String, var registrationTime: Int) {
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
+
+class User(private val name: String, private val login: String, private val mail: String) {
+  private val DATE_FORMAT = "EEE, MMM dd, yyyy h:mm a"
+  private val registrationTime = getDateAsString(Calendar.getInstance.getTime)
+
+  def getDateAsString(d: Date): String = {
+    val dateFormat = new SimpleDateFormat(DATE_FORMAT)
+    dateFormat.format(d)
+  }
+
+  def getName: String = name
+
+  def getLogin: String = login
+
+  def getMail: String = mail
+
+  def getRegistrationTime: String = registrationTime
+
   override def toString: String =
-    s"User: ($name, $login, $mail, $registrationTime)"
+    s"User: ($name, $login, $mail, $getRegistrationTime)"
 }
